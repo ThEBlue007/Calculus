b83e6e73e440b46d2d7414348a87a1130a5aafc1
ThEBlue007 <santod0946919475@gmail.com>
2026-07-15 03:51:46 +0700

    Fix Vercel serverless routing by moving files out of api folder

--- api/index.js
+++ api/index.js
@@ -1,26 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { connectDB } = require('./config/db');
-const apiRoutes = require('./routes/api');
+const { connectDB } = require('../server/config/db');
 
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api', apiRoutes);
+app.use('/api', require('../server/routes/api'));
 
-// Only listen if not running on Vercel (local dev)
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-}
-
-// Export for Vercel
 module.exports = app;
