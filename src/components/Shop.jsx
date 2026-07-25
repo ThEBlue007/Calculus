3d7edcaff86e34ce15c8cc3c539240a42dd1c874
ThEBlue007 <santod0946919475@gmail.com>
2026-07-25 18:47:13 +0700

    Fix Shop crash and KaTeX rendering bugs

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { GameContext } from '../context/GameContext';
 
 export default function Shop({ onBack }) {
